18d0362584814299b575877903801de3bf821e85
Alejandro Lopez <alejeindrox@hotmail.com>
2019-08-25 19:13:40 -0400

    Updates

---
+++ precache-manifest.01effdc9c98a9f675ccfa6b5504eef3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4336d9c4c02e711ef1b0f5b834de4a9f",
+    "url": "/Cinema/index.html"
+  },
+  {
+    "revision": "54ef4357585d3a596843",
+    "url": "/Cinema/static/css/main.95f1dfe1.chunk.css"
+  },
+  {
+    "revision": "19d638edf1411d668111",
+    "url": "/Cinema/static/js/2.b22631f4.chunk.js"
+  },
+  {
+    "revision": "54ef4357585d3a596843",
+    "url": "/Cinema/static/js/main.2a83dc05.chunk.js"
+  },
+  {
+    "revision": "c032624440b27d05b0f8",
+    "url": "/Cinema/static/js/runtime~main.efd9e64a.js"
+  }
+]);
