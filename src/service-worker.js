64ece00e8e19b0a9ada3f6594a525e6d852faaa4
Alejandro Lopez <alejeindrox@hotmail.com>
2019-08-26 00:43:21 -0400

    Fixed workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,8 +5,6 @@
  */
 
 // Preload App
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
-
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
